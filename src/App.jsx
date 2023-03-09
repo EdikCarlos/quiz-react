2e0a55faa80194355b061746670d7031d283d9b4
unknown <edik carlos>
2023-03-09 15:21:06 -0300

    creating context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Welcome from './components/Welcome'
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
 
 function App() {
+  const quizTitle = useContext(QuizContext);
+
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>{quizTitle.title}</h1>
       <Welcome />
     </div>
   )
